2d517ef83969b1f428faf40e3b1c9291324368a5
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-12 18:57:55 +0900

    setting csrf

--- go.mod
+++ go.mod
@@ -3,5 +3,9 @@ module movie-info-backend
 go 1.14
 
 require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/csrf v1.7.0
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
+	github.com/lib/pq v1.7.0
 )
--- main.go
+++ main.go
@@ -4,21 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func signup(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("signup関数実行")
+	w.Write([]byte("successfully called signup"))
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("login関数実行")
+	w.Write([]byte("successfully called login"))
 }
 
+
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/singup", signup).Methods("POST")
-	router.HandleFunc("/login", login).Methods("POST")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/singup", signup).Methods("POST")
+	r.HandleFunc("/login", login).Methods("POST")
+
 	log.Println("サーバー起動 : 8000 port で受信")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000",
+		handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"*"}))(r)))
+	http.Handle("/", r)
 }
