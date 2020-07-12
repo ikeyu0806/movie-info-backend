de9aeb466e6ed9fb382e461cc634b389fe7a47fc
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-12 11:38:09 +0900

    implement http router

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/gorilla/handlers"
 )
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("signup関数実行")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("login関数実行")
+}
+
 func main() {
-	
+	router := mux.NewRouter()
+	router.HandleFunc("/singup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	log.Println("サーバー起動 : 8000 port で受信")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
