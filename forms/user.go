f8b3f7cf9c752575d5cca78581b3e397617ce45c
ikeyu0806 <ikeyu0806@gmail.com>
2020-07-16 21:53:19 +0900

    modify go.sum

--- forms/user.go
+++ forms/user.go
@@ -1,7 +1,7 @@
 package forms
 
 type UserSignup struct {
-	Name     string `json:"name" binding:"required"`
-	EMail string `json:"birthday" binding:"required"`
+	Name       string `json:"name" binding:"required"`
+	EMail      string `json:"birthday" binding:"required"`
 	Password   string `json:"gender" binding:"required"`
 }
