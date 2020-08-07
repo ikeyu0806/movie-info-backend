1fa26c5ef261b3c5a362f9eb7d14c7f7acd37896
ikeyu0806 <ikeyu0806@gmail.com>
2020-08-07 10:50:30 +0900

    add model test

--- go.mod
+++ go.mod
@@ -7,5 +7,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
+	github.com/gorilla/handlers v1.4.2
+	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.15
 )
--- models/review_test.go
+++ models/review_test.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 	"testing"
   "github.com/gin-gonic/gin"
-  "net/http"
+	"net/http"
+	"bytes"
 )
 
-func TestFindByMovieID(t *testing.T) {
+func TestCreateReview(t *testing.T) {
+	jsonStr := `{"public_id":123456789, "comment":"good", "score":12345}`
+	req, _ := http.NewRequest("POST", "/review/create", bytes.NewBuffer([]byte(jsonStr)))
+
+	var context *gin.Context
+	context = &gin.Context{Request: req}
+	
+	var r ReviewModel
+	result, err := r.CreateReview(context)
+
+	fmt.Println(result)
+	if err != nil {
+		fmt.Println(err)
+		t.Fatal(err)
+	}
+}
+
+func TestFindByMovieId(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/reviews/1", nil)
 
 	var context *gin.Context
