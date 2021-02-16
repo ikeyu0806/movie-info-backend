ed55ac2729ab956d5241e6ea2faccf18a0b8a3b5
ikeyu0806 <ikeyu0806@gmail.com>
2021-02-16 17:40:55 +0900

    add db env switching

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
     "log"
+    "os"
     "io/ioutil"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
@@ -26,18 +27,53 @@ func GetDB() (*gorm.DB, error) {
       log.Fatal(err)
   }
 
-  DBMS     := "mysql"
-  USER     := m["development"].(map[interface {}]interface {})["user"].(string)
-  PASS     := m["development"].(map[interface {}]interface {})["password"].(string)
-  PROTOCOL := m["development"].(map[interface {}]interface {})["protocol"].(string)
-  DBNAME   := m["development"].(map[interface {}]interface {})["db_name"].(string)
+  env := os.Getenv("SERVICE_ENV")
 
-  CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME+"?parseTime=true"
-  db,err := gorm.Open(DBMS, CONNECT)
-
-  if err != nil {
-    panic(err.Error())
+  switch env {
+  case "production":
+    DBMS     := "mysql"
+    USER     := m["prooduction"].(map[interface {}]interface {})["user"].(string)
+    PASS     := m["prooduction"].(map[interface {}]interface {})["password"].(string)
+    PROTOCOL := m["prooduction"].(map[interface {}]interface {})["protocol"].(string)
+    DBNAME   := m["prooduction"].(map[interface {}]interface {})["db_name"].(string)
+  
+    CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME+"?parseTime=true"
+    db,err := gorm.Open(DBMS, CONNECT)
+  
+    if err != nil {
+      panic(err.Error())
+    }
+    log.Println("connect mysql")
+    return db, err
+  case "test":
+    DBMS     := "mysql"
+    USER     := m["test"].(map[interface {}]interface {})["user"].(string)
+    PASS     := m["test"].(map[interface {}]interface {})["password"].(string)
+    PROTOCOL := m["test"].(map[interface {}]interface {})["protocol"].(string)
+    DBNAME   := m["test"].(map[interface {}]interface {})["db_name"].(string)
+  
+    CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME+"?parseTime=true"
+    db,err := gorm.Open(DBMS, CONNECT)
+  
+    if err != nil {
+      panic(err.Error())
+    }
+    log.Println("connect mysql")
+    return db, err
+  default:
+    DBMS     := "mysql"
+    USER     := m["development"].(map[interface {}]interface {})["user"].(string)
+    PASS     := m["development"].(map[interface {}]interface {})["password"].(string)
+    PROTOCOL := m["development"].(map[interface {}]interface {})["protocol"].(string)
+    DBNAME   := m["development"].(map[interface {}]interface {})["db_name"].(string)
+  
+    CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME+"?parseTime=true"
+    db,err := gorm.Open(DBMS, CONNECT)
+  
+    if err != nil {
+      panic(err.Error())
+    }
+    log.Println("connect mysql")
+    return db, err
   }
-  log.Println("connect mysql")
-  return db, err
 }
